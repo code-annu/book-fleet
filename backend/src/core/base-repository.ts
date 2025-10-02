a6790dca153690a19600f2b255b9c346513c119b
Anurag Rajput <code.annu@gmail.com>
2025-10-02 21:16:23 +0530

    Add base functions to base repository

--- backend/src/core/base-repository.ts
+++ backend/src/core/base-repository.ts
@@ -5,8 +5,58 @@ export class BaseRepository<T> {
   constructor(protected readonly table: string) {}
 
   protected async insert(data: T): Promise<T> {
-    const result = await this.db.from(this.table).insert(data).select();
-    // result.
+    const result = await this.db
+      .from(this.table)
+      .insert(data)
+      .select()
+      .single();
+    if (result.error) throw result.error;
+    return result.data;
+  }
+
+  protected async getById(id: string | number): Promise<T | null> {
+    const { data, error } = await this.db
+      .from(this.table)
+      .select("*")
+      .eq("id", id)
+      .single();
+    if (error) {
+      throw error;
+    }
     return data;
+  }
+
+  protected async updateById(
+    id: string | number,
+    updates: Partial<Omit<T, "id">>
+  ): Promise<T> {
+    const { data, error } = await this.db
+      .from(this.table)
+      .update(updates)
+      .eq("id", id)
+      .select()
+      .single();
+    if (error) throw error;
+    return data!;
+  }
+
+  protected async deleteById(id: string | number): Promise<T> {
+    const { data, error } = await this.db
+      .from(this.table)
+      .delete()
+      .eq("id", id)
+      .select()
+      .single();
+    if (error) throw error;
+    return data!;
+  }
+
+  protected async listByIds(ids: Array<string | number>): Promise<T[]> {
+    const { data, error } = await this.db
+      .from(this.table)
+      .select("*")
+      .in("id", ids);
+    if (error) throw error;
+    return data || [];
   }
 }
