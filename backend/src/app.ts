10190f4282c841705eb58e5829b0e0e915224389
Anurag Rajput <code.annu@gmail.com>
2025-10-07 21:06:58 +0530

    add app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { authRouter } from "./api/router/auth-router";
+
+const app = express();
+const BASE_URL = "/api/v1";
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(`${BASE_URL}/auth`, authRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening at: http://localhost:${PORT}${BASE_URL}`);
+});
