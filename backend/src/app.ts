937620846977391f5e76814237706c8e6aa7e804
Anurag Rajput <code.annu@gmail.com>
2025-10-11 21:37:50 +0530

    implement book sale feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { authRouter } from "./api/router/auth-router";
 import { userProfileRouter } from "./api/router/user-profile-router";
 import { validateAuthorization } from "./api/middleware/auth-middleware";
+import { bookSaleRouter } from "./api/router/book-sale-router";
 
 const app = express();
 const BASE_URL = "/api/v1";
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(`${BASE_URL}/auth`, authRouter);
 app.use(`${BASE_URL}/profile`, validateAuthorization, userProfileRouter);
+app.use(`${BASE_URL}/book-sale`, validateAuthorization, bookSaleRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
